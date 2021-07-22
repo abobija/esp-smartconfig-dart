01ddf8ebbeb14a7dd6b4e856488b352c1b8623da
abobija <abobijax@gmail.com>
2021-07-22 14:54:10 +0200

    Exceptions inline docs

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -1,4 +1,4 @@
-/// General EspSmartconfig exception
+/// Generic EspSmartconfig exception
 abstract class EspSmartConfigException implements Exception {
   /// Message
   final String? message;
@@ -9,14 +9,18 @@ abstract class EspSmartConfigException implements Exception {
   String toString() => message ?? super.toString();
 }
 
+/// Generic provisioning exception
 class ProvisioningException extends EspSmartConfigException {
   ProvisioningException([String? message]) : super(message);
 }
 
+/// Error that will be thrown on try to add response
+/// that already exists in the list of protocol responses
 class ProvisioningResponseAlreadyReceivedError extends ProvisioningException {
   ProvisioningResponseAlreadyReceivedError([String? message]) : super(message);
 }
 
+/// Invalid provisioning reponse exception
 class InvalidProvisioningResponseDataException extends ProvisioningException {
   InvalidProvisioningResponseDataException([String? message]) : super(message);
 }
